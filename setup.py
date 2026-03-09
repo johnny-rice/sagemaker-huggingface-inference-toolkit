4320a21358fc23a31b53ab0b2d4e4e31b05b6841
Enrique Hernández Calabrés <ehcalabres@gmail.com>
2026-03-09 10:34:16 +0100

    Update min Python version to 3.10 to match Transformers v5

--- setup.py
+++ setup.py
@@ -71,7 +71,7 @@ extras["test"] = [
     "psutil",
     "datasets",
     "pytest-sugar",
-    "black==21.4b0",
+    "black==22.3.0",
     "sagemaker",
     "boto3",
     "mock==2.0.0",
@@ -80,7 +80,7 @@ extras["test"] = [
 extras["benchmark"] = ["boto3", "locust"]
 
 extras["quality"] = [
-    "black>=21.10",
+    "black>=22.3.0",
     "isort>=5.5.4",
     "flake8>=3.8.3",
 ]
@@ -103,7 +103,7 @@ setup(
     install_requires=install_requires,
     extras_require=extras,
     entry_points={"console_scripts": "serve=sagemaker_huggingface_inference_toolkit.serving:main"},
-    python_requires=">=3.6.0",
+    python_requires=">=3.10.0",
     license="Apache License 2.0",
     classifiers=[
         "Development Status :: 5 - Production/Stable",
@@ -113,8 +113,9 @@ setup(
         "License :: OSI Approved :: Apache Software License",
         "Operating System :: OS Independent",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Programming Language :: Python :: 3.12",
         "Topic :: Scientific/Engineering :: Artificial Intelligence",
     ],
 )
